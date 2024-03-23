98facb7c7b5f554284d47c8be4cf2bd716976f31
Roshdy <126667610+poshdy@users.noreply.github.com>
2024-03-23 13:31:07 -0700

    feat: pre

--- events.js
+++ events.js
@@ -2,9 +2,19 @@ const events = require("node:events");
 
 const messi = new events();
 
-
-messi.on("hi", () => {
-  console.log("hi from on");
+messi.prependListener("hi", () => {
+  console.log("Hi from  prepend");
+});
+// messi.on("hi", () => {
+//   console.log("Hi from on ");
+// });
+messi.on("hello", () => {
+  console.log("Hello");
 });
 
+// messi.on("hi", () => {
+//   console.log("hi from on");
+// });
+
 messi.emit("hi");
+messi.emit("hello");
