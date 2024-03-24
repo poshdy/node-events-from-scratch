8063d17e017196d1d4b4f2d0e2f321d8c2b9b49d
Roshdy <126667610+poshdy@users.noreply.github.com>
2024-03-23 18:24:59 -0700

    instantiate NewEmitter Class

---
+++ events-from-scratch.js
@@ -0,0 +1,24 @@
+class NewEmitter {
+  events = {};
+
+  // on(){}
+
+  // once(){}
+
+  // off(){}
+
+//   RemoveEvent(event, fn) {
+//     const Event = this.events[event];
+//     if (Event) return `There is no event called ${event}`;
+//   }
+
+//   AddEvent(event, fn) {
+//     this.events[event] = this.events || [];
+//     this.events[event].push(fn);
+//     return this;
+//   }
+
+  // product(){}
+
+  // AddEvent(){}
+}
